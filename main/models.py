8bce8fd2f39f5ed2f1d7d2c89d19f40b0927c3b6
eljamm <83901271+eljamm@users.noreply.github.com>
2022-07-04 12:52:35 +0100

    main: Combine date and time fields

--- main/models.py
+++ main/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
+
 class Sensor(models.Model):
     name = models.CharField(max_length=150)
-    
+
     def __str__(self):
         return self.name
 
 
 class Item(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
     data = models.CharField(max_length=300)
-    time = models.TimeField()
 
     def __str__(self):
         return self.data
