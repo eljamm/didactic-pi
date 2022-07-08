2a95e453d1da798bd6ce320674ece78cec245074
eljamm <83901271+eljamm@users.noreply.github.com>
2022-07-08 14:23:05 +0100

    django: Add sensor websocket routing to asgi.py

--- app/asgi.py
+++ app/asgi.py
@@ -9,13 +9,25 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 django_asgi_app = get_asgi_application()
 
+import sensor.routing
+
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+  "http": django_asgi_app,
+  "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                sensor.routing.websocket_urlpatterns
+            )
+        )
+    ),
 })
