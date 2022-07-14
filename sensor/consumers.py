617da5816ea6b4a2ee83aa4e44679632f90a6a61
eljamm <83901271+eljamm@users.noreply.github.com>
2022-07-14 09:40:09 +0100

    Update websocket consumers

--- sensor/consumers.py
+++ sensor/consumers.py
@@ -1,25 +1,34 @@
 import json
+import logging
+
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from channels.generic.websocket import (AsyncWebsocketConsumer,
+                                        WebsocketConsumer)
+from django.utils import timezone
+from .models import Sensor, Raspi
 
 
-class ReadConsumer(AsyncWebsocketConsumer):
+class PiConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.sensor_name = self.scope['url_route']['kwargs']['sensor_name']
-        self.sensor_group_name = 'sensor_%s' % self.sensor_name
+        self.pi_name = self.scope['url_route']['kwargs']['pi_name']
+        self.pi_group_name = 'pi_%s' % self.pi_name
 
-        # Join sensor group
+        # Join Pi group
         await self.channel_layer.group_add(
-            self.sensor_group_name,
+            self.pi_group_name,
             self.channel_name
         )
 
         await self.accept()
 
+    def get_name(self, name):
+        return Raspi.objects.get(name=name).name
+
     async def disconnect(self, close_code):
-        # Leave sensor group
+        # Leave Pi group
         await self.channel_layer.group_discard(
-            self.sensor_group_name,
+            self.pi_group_name,
             self.channel_name
         )
 
@@ -27,21 +36,25 @@ class ReadConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
 
         # Send message to group
         await self.channel_layer.group_send(
-            self.sensor_group_name,
+            self.pi_group_name,
             {
                 'type': 'sensor_message',
-                'message': message
+                'message': message,
+                'time': now.isoformat()
             }
         )
 
     # Receive message from group
     async def sensor_message(self, event):
         message = event['message']
+        now = event['time']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'time': now
         }))
--- sensor/routing.py
+++ sensor/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/sensor/read/(?P<sensor_name>\w+)/$',
-            consumers.ReadConsumer.as_asgi()),
+    re_path(r'ws/(?P<pi_name>\w+)/$',
+            consumers.PiConsumer.as_asgi()),
 ]
