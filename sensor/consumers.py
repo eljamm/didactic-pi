27df610b90fc8988a392a9364ed3dfcac2a6b82f
eljamm <83901271+eljamm@users.noreply.github.com>
2022-07-22 17:57:39 +0100

    Update WebSocket configuration

--- sensor/consumers.py
+++ sensor/consumers.py
@@ -6,7 +6,7 @@ from channels.db import database_sync_to_async
 from channels.generic.websocket import (AsyncWebsocketConsumer,
                                         WebsocketConsumer)
 from django.utils import timezone
-from .models import Sensor, Raspi
+from .models import Sensor, Raspi, Item
 
 
 class PiConsumer(AsyncWebsocketConsumer):
@@ -36,25 +36,80 @@ class PiConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        
+        sensor = ''
+        dht11_json = ''
         now = timezone.now()
+
+        try:
+            sensor = text_data_json['sensor']
+        except Exception as e:
+            logging.debug(e.args[0])
+        finally:
+            generic_json = {
+            'type': 'sensor_message',
+            'sensor': sensor,
+            'message': message,
+            'time': now.isoformat()
+        }
+
+        if (sensor == 'dht11'):
+            temp = text_data_json['temp']
+            hum = text_data_json['hum']
+
+            dht11_json = {
+                'temp': temp,
+                'hum': hum,
+            }
+
+            json_file = {**generic_json, **dht11_json}
+
+        else:
+            json_file = generic_json
 
         # Send message to group
         await self.channel_layer.group_send(
-            self.pi_group_name,
-            {
-                'type': 'sensor_message',
-                'message': message,
-                'time': now.isoformat()
-            }
+            self.pi_group_name, json_file
         )
 
     # Receive message from group
     async def sensor_message(self, event):
         message = event['message']
+        sensor = event['sensor']
         now = event['time']
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        # Write message to database
+        await self.register_data(self.pi_name, sensor, now, message)
+
+        generic_json = {
+            'sensor': sensor,
             'message': message,
             'time': now
-        }))
+        }
+
+        if (sensor == 'dht11'):
+            temp = event['temp']
+            hum = event['hum']
+
+            dht11_json = {
+                'temp': temp,
+                'hum': hum,
+            }
+
+            json_file = {**generic_json, **dht11_json}
+
+        else:
+            json_file = generic_json
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps(json_file))
+
+    @database_sync_to_async
+    def register_data(self, raspi, sensor, time, data):
+        try:
+            pi = Raspi.objects.get(name=raspi)
+            se = pi.sensor_set.get(name=sensor)
+            se.item_set.create(datetime=time, data=data)
+        except:
+            #logging.debug("Raspi: %s | Sensor: %s | Time: %s | Data: %s" % (raspi, sensor, time, data))
+            logging.debug("Sensor does not exist in database")
--- sensor/routing.py
+++ sensor/routing.py
@@ -5,4 +5,6 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'ws/(?P<pi_name>\w+)/$',
             consumers.PiConsumer.as_asgi()),
+    re_path(r'ws/(?P<pi_name>\w+)/(?P<sensor_name>\w+)/$',
+            consumers.PiConsumer.as_asgi()),
 ]
