9b6c7c8130f457fbeaaf0175a321e14e90d3b2ea
eljamm <83901271+eljamm@users.noreply.github.com>
2022-07-28 16:56:33 +0100

    django: Get allowed hosts from .env file

--- app/settings.py
+++ app/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    'localhost',
-]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
