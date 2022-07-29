74f0a296d15f4af20c8c69363986a694e62ec7cb
eljamm <83901271+eljamm@users.noreply.github.com>
2022-07-29 19:38:21 +0100

    Add a better filter for titles

--- sensor/templatetags/reusable_blocks.py
+++ sensor/templatetags/reusable_blocks.py
@@ -1,7 +1,25 @@
 from django import template
+from django.template.defaultfilters import title as default_title
 from .range_iterator import make_list
 
 register = template.Library()
+
+
+@register.filter(is_safe=True)
+def better_title(name: str):
+    title = {
+        "dht11": "DHT11",
+        "8x8matrix": "LED Matrix 8x8",
+        "lcd": "LCD",
+        "7segment": "Seven Segment Display",
+        "ledarray": "LED Array",
+        "dpad": "D-Pad",
+    }
+
+    if name in title:
+        return title[name]
+    else:
+        return default_title(name)
 
 
 @register.inclusion_tag('sensor/blocks/extra.html', takes_context=True)
