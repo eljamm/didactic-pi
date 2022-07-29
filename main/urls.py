4dbac9e89e67cee9b2aa1e161e3413bf10cbc414
eljamm <83901271+eljamm@users.noreply.github.com>
2022-07-29 19:43:40 +0100

    django: Remove unnecessary app

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:id>", views.sensor_id, name="sensors"),
-    path("add_data/", views.add_data, name="add_data"),
-    path("add_sensor/", views.add_sensor, name="add_sensor"),
-]
