c0b9675016d1f26a3c730b8cafc2a1c4094ca1cb
eljamm <83901271+eljamm@users.noreply.github.com>
2022-08-17 22:25:23 +0100

    Add 7 segment webpage

---
+++ sensor/static/sensor/js/sensors/segment.js
@@ -0,0 +1,90 @@
+window.onload = function () {
+    const debug = JSON.parse(document.getElementById('debug').textContent);
+    const piName = JSON.parse(document.getElementById('pi-name').textContent);
+    const sensorName = JSON.parse(document.getElementById('sensor-name').textContent);
+
+    const dataSocket = new WebSocket(
+        'ws://'
+        + window.location.host
+        + '/ws/'
+        + piName
+        + '/'
+        + sensorName
+        + '/'
+    );
+
+    dataSocket.onmessage = function (e) {
+        const data = JSON.parse(e.data);
+        console.log(data);
+    };
+
+    dataSocket.onopen = function (e) {
+        dataSocket.send(JSON.stringify({
+            'sensor': sensorName,
+            'message': 'start',
+            'message_type': 'command'
+        }));
+    }
+
+    window.onbeforeunload = function () {
+        dataSocket.send(JSON.stringify({
+            'sensor': sensorName,
+            'message': 'stop',
+            'message_type': 'command'
+        }));
+        dataSocket.close
+    };
+
+    dataSocket.onclose = function (e) {
+        console.error('Data socket closed unexpectedly');
+    };
+
+    document.querySelector('#seg-data-message-input').onkeyup = function (e) {
+        if (e.keyCode === 13) {  // enter, return
+            document.querySelector('#seg-data-message-submit').click();
+        }
+    };
+
+    document.querySelector('#seg-data-message-submit').onclick = function (e) {
+        const messageInputDom = document.querySelector('#seg-data-message-input');
+        const message = messageInputDom.value;
+        dataSocket.send(JSON.stringify({
+            'sensor': sensorName,
+            'message': message,
+            'message_type': 'data'
+        }));
+        messageInputDom.value = '';
+    };
+
+    document.querySelector('#seg-clear-textarea').onclick = function (e) {
+        const dataLogDom = document.querySelector('#seg-data-message-input');
+        dataLogDom.value = '';
+    };
+
+    if (debug === true) {
+        document.querySelector('#data-message-input').focus();
+        document.querySelector('#data-message-input').onkeyup = function (e) {
+            if (e.keyCode === 13) {  // enter, return
+                document.querySelector('#data-message-submit').click();
+            }
+        };
+
+        document.querySelector('#data-message-submit').onclick = function (e) {
+            const messageInputDom = document.querySelector('#data-message-input');
+            const message = messageInputDom.value;
+            dataSocket.send(JSON.stringify({
+                'sensor': sensorName,
+                'message': message,
+                'message_type': 'command'
+            }));
+            messageInputDom.value = '';
+        };
+
+        document.querySelector('#clear-textarea').onclick = function (e) {
+            const dataLogDom = document.querySelector('#data-log');
+            dataLogDom.value = '';
+        };
+    }
+}
+
+
